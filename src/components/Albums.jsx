c0d89904838dca1c14abe495006642c604cd2f1f
Luke Li <leezhc@gmail.com>
2017-05-24 01:02:00 -0700

    route example update

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -3,26 +3,38 @@ import { Link, Route, Switch } from 'react-router-dom';
 
 class About extends Component {
     render() {
-        const Content = (props) => (
+        const Content = () => (
             <div style={{backgroundColor: 'lightblue'}}>
+                <Link to="/albums">Albums</Link>
+                <hr />
                 <p>This Part is nested router</p>
                 <p>album name...</p>
                 <div>
                     name from Path params: 
-                    <span style={{backgroundColor: 'red', fontSize:'20px'}}>{props.match.params.name}</span>
+                    {/* this component hasn't imported {match} so using this.props.match */}
+                    <span style={{backgroundColor: 'red', fontSize:'20px'}}>{this.props.match.params.name}</span>
                 </div>
             </div>
         );
+
+        const mainAlbum = ({match}) => (
+            <div>
+                <p>
+                    Albums main component, should show all albums list
+                </p>
+                {/* using {match} to inherit this.props.match */}
+                <Link to={`${match.url}/new`}>New Album Page</Link>
+                <hr />
+            </div>
+        );
+
         return (
             <div className="Albums">
                 <Link to="/">HOME</Link>
                 <hr />
-                <p>
-                    Albums main component
-                </p>
-                <hr />
                 <Switch>
-                    <Route path='/albums/:name' component={Content} />
+                    <Route exact path={this.props.match.url} component={mainAlbum} />
+                    <Route path={`${this.props.match.url}/:name`} component={Content} />
                 </Switch>
             </div>
         );
