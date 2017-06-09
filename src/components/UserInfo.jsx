a61d9250b470af4f1c98deb42bef1dc95fe05d09
Luke Li <leezhc@gmail.com>
2017-06-08 22:41:36 -0700

    userinfo modify and sass

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import "../sass/UserInfo.scss";
+
+class logC extends Component {
+    state = { inputValue: '' }
+    render() {
+        return (
+            <div className="userInfo">
+                <h2>User Info</h2>
+                <p>This component is for user info</p>
+                <textarea rows={'5'} value={this.state.inputValue} onChange={(e)=>this.setState({inputValue: e.target.value})}/>
+                <hr />
+                <button className='btn btn-defaults' onClick={()=>{alert(this.state.inputValue)}}>Save</button>
+                <p>{this.state.inputValue}</p>
+            </div>
+        );
+    }
+}
+
 
 const userInfo = () => (
     <div>
-        <h2>User Info</h2>
-        <p>This component is for user info</p>
+        
+        <logC></logC>
     </div>
 );
 
-export default userInfo;
+function logComponent() {
+    return (
+        <div>
+            <textarea onChange={(e)=>console.log(e.target.value)}/>
+            <button onClick={()=>{console.log('save button')}}>Save</button>
+        </div>
+    )
+}
+
+
+export default logC;
