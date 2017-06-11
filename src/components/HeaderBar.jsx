87d80833f108da84c15b87f9bec8e977a251f586
Luke Li <leezhc@gmail.com>
2017-06-11 00:53:23 -0700

    Headerbar ligin logout with fakeauth

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,29 +1,20 @@
 import React, {Component} from 'react';
 import LoginStatus from './LoginStatus';
-import {fakeAuth} from '../routes';
 import logo from '../logo.svg';
 require('./HeaderBar.css');
 
 class HeaderBar extends Component {
 //    state = {  }
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date(), auth: this.props.auth};
-    }
 
     render() {
-        let data = this.state.date.toJSON;
-        let fakeAuth1 = {...fakeAuth};
+        
         return (
             <div className="HeaderBar">
                 <img src={logo} className="App-logo col-md-3" alt="logo" />
                 <span className="col-md-6 title">Welcome to React header section</span>
                 <span className="col-md-3 logbtn">
-                    {this.state.auth?'log in':<LoginStatus />}
+                    <LoginStatus />
                 </span>
-                <span>{data}</span>
-                <button onClick={()=> console.log(fakeAuth1.isAuthenticated+this.state.date)}>test</button>
-                {fakeAuth1.isAuthenticated? <span>TRUE</span>: <span>FALSE</span>}
             </div>
         );
     }
--- src/components/LoginStatus.jsx
+++ src/components/LoginStatus.jsx
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
 import { withRouter } from 'react-router-dom'; // withRouter can let component access to history object
+import {fakeAuth} from '../routes.js';
 
 class LoginStatus extends Component {
-    state = {  }
+
+    constructor () {
+        super();
+        this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+        let timer = setInterval(()=>{this.setState({date: new Date()})}, 500);
+        this.setState({timer: timer})
+    }    
+
+    componentWillUnmount() {
+        clearInterval(this.state.timer)
+    }
 
     render() {
         const buttonNavi = () => {
@@ -10,16 +24,33 @@ class LoginStatus extends Component {
             this.props.history.push('/login');
         }
 
+        let fakeAuth1 = {...fakeAuth};
+
+        const signUp = () => {
+            alert('build sign up page'); // TODO BUILD SIGN UP COMPONENT
+        }
+
+        const logOut = () => {
+            fakeAuth.signout();
+            this.props.history.push('/');
+        }
         return (
             <div>
-                <div>
-                    <button onClick={() => buttonNavi()} className="btn btn-success headbtn">Login</button>
-                    <button className="btn btn-primary headbtn">Sign up</button>
-                </div>
-                <div>
-                    <span>User Name</span>
-                    <button className="btn btn-primary headbtn">Log out</button>
-                </div>
+                {
+                    !fakeAuth1.isAuthenticated ?
+                    (<div>
+                        <button onClick={() => buttonNavi()} className="btn btn-success headbtn">Login</button>
+                        <button className="btn btn-primary headbtn" onClick={()=>signUp()}>Sign up</button>
+                    </div>)
+                    :
+                    (
+                    <div>
+                        <span>Welcome, User Name</span>
+                        <button className="btn btn-primary headbtn" onClick={()=>logOut()}>Log out</button>
+                        <p>{this.state.date.toJSON()}</p>
+                    </div>
+                    )
+                }
             </div>
         );
     }
