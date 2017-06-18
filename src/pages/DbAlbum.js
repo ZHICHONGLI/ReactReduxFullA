f112529a87fc481ce1fec91c48a54b404b12fda6
Luke Li <leezhc@gmail.com>
2017-06-17 23:57:29 -0700

    DbALbum sass added

--- src/pages/DbAlbum.js
+++ src/pages/DbAlbum.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import fetch from 'isomorphic-fetch';
 // import actions from '../actions/index';
-import '../sass/AlbumContent.scss';
+import '../sass/DbAlbum.scss';
 
 class dbAlbum extends Component {
     constructor(props) {
@@ -21,7 +21,7 @@ class dbAlbum extends Component {
         ).then(response => {
             console.log(response);
             this.setState({Album: response});
-            document.title = response.alt_title+' '+response.author[0].name;
+            document.title = response.attrs.title[0]+' '+ response.alt_title+' '+response.author[0].name;
         }  
         ).catch(err => {
             console.log(err)
@@ -31,7 +31,7 @@ class dbAlbum extends Component {
     }
     render() {
         return (
-            <div>
+            <div className="db-album">
                 <Link to="/albums">Albums</Link>
                 <hr />
                 <p>This Part is nested router</p>
@@ -44,28 +44,19 @@ class dbAlbum extends Component {
                 <div>
                     <p>Fetched from douban by API</p>
                     <button onClick={()=>console.log(this.state)}>test</button>
-                    {/*
+                    {
                         (this.state.Album === undefined) ?
-                        (<h2>Album Cannot Found...</h2>) 
+                        (<h2>Loading...</h2>) 
                         : (<div>
-                                <p>TITLE: {this.state.Album.alt_title}</p>
+                                <p>TITLE: {this.state.Album.attrs.title[0]}</p>
                                 <p>ARTIST: {this.state.Album.author[0].name}</p>
-                                <p>YEAR: {this.state.Album.pubdate}</p>
+                                <p>YEAR: {this.state.Album.attrs.pubdate[0]}</p>
                             </div>)
-                    */}
+                    }
                 </div>
             </div>
         );
     }
 }
 
-/*
-const mapStateToProps = state => ({
-    Album: state.totalList.currentAlbum
-})
-
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators(actions, dispatch)
-})
-*/
 export default dbAlbum;
