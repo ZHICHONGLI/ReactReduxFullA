c98196af86bd606b5949bde6433f2d612a11e955
Luke Li <leezhc@gmail.com>
2017-06-18 22:18:20 -0700

    DbAlbum update

--- src/pages/DbAlbum.js
+++ src/pages/DbAlbum.js
@@ -35,22 +35,25 @@ class dbAlbum extends Component {
                 <Link to="/albums">Albums</Link>
                 <hr />
                 <p>This Part is nested router</p>
-                <p>album name...</p>
                 <div>
-                    name from Path params: 
+                    douban id from Path params: 
                     <span style={{backgroundColor: 'red', fontSize:'20px'}}>{this.props.match.params.id}</span>
                 </div>
                 <hr />
                 <div>
                     <p>Fetched from douban by API</p>
-                    <button onClick={()=>console.log(this.state)}>test</button>
+                    <button onClick={()=>console.log(this.state)}>test console douban response</button>
                     {
                         (this.state.Album === undefined) ?
                         (<h2>Loading...</h2>) 
-                        : (<div>
-                                <p>TITLE: {this.state.Album.attrs.title[0]}</p>
-                                <p>ARTIST: {this.state.Album.author[0].name}</p>
-                                <p>YEAR: {this.state.Album.attrs.pubdate[0]}</p>
+                        : (<div className="row">
+                                <div className="col-sm-1 col-sm-offset-2"><img src={this.state.Album.image} className='img-thumbnail' /></div>
+                                <div className="col-sm-6">
+                                    <p>Title: {this.state.Album.attrs.title[0]}</p>
+                                    <p>Artist: {this.state.Album.author[0].name}</p>
+                                    <p>Year: {this.state.Album.attrs.pubdate[0]}</p>
+                                    <p>Rating: {this.state.Album.rating.average} ({this.state.Album.rating.numRaters})</p>
+                                </div>                               
                             </div>)
                     }
                 </div>
